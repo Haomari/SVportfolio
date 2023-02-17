05a2e08f860aadbca336513976be249a14fbc1fe
Haomari <tcurpal@gmail.com>
2023-02-17 19:26:02 +0100

    learning-section(1.01[html, scss, js])
    trying to do hovers in js

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,4 +1,11 @@
-// Подключение функционала "Чертогов Фрилансера"
-import { isMobile } from "./functions.js";
-// Подключение списка активных модулей
-import { flsModules } from "./modules.js";
+
+let questionsSectionText = document.querySelectorAll(".questions-section__text");
+let questionsSectionIcon = document.querySelectorAll(".questions-section__icon");
+console.log(document.querySelectorAll(".questions-section__icon"))
+
+function iconHover() {
+	console.log("i'm good")
+	questionsSectionIcon.classList.add("._hover");
+};
+
+questionsSectionText.addEventListener("click", iconHover());
